d82e63177d183e3f830440b4586b69d9ce0df16e
pyyyy <72590023+one-pyy@users.noreply.github.com>
2022-06-07 15:42:43 +0800

    Update and rename robrobrob to robrobrob.py

--- robrobrob.py
+++ robrobrob.py
@@ -7,9 +7,14 @@ header='''
 ???
 '''
 
-while 1: 
+if idList.__len__()==0:
+    break
   for id in idList:
     js=json.loads(HackRequests.http('https://webproxy.dhu.edu.cn/http/77726476706e69737468656265737421fae0469069346045300d8db9d6562d/dhu/selectcourse/scSubmit?vpn-12-o1-jwgl.dhu.edu.cn', headers=header, post={'cttId':id,'needMaterial':False, 'capCode':'fgee'}).text())
     js['id']=id
-    print(js)
+    if js['success']==True:
+      print(f'抢到了{id}')
+      idList.remove(id)
+      break
+    print(js, end='                \r')
     time.sleep(5)
