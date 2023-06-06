2bd1edb37727f658fea08df046c44fd2a4ea67cc
pyyyy <72590023+one-pyy@users.noreply.github.com>
2023-06-06 21:01:51 +0800

    Update robrobrob.py

--- robrobrob.py
+++ robrobrob.py
@@ -4,30 +4,32 @@ import time
 import random
 from typing import *
 import traceback
+from objprint import op
 
-#course_code, tc_code, del_tc_code
-id_list = [(131841, 263331, 263330)]
+# 考虑某节课选了某老师不满意, 想要换老师但怕退了不能选上的情况
+# 可以在发现有空余时再退课并选课
+#course_code, tc_code, del_tc_code(可以是None)
+id_list = [('039661', '265177', None)]
 #https://webproxy.dhu.edu.cn/http/446a5061214023323032323131446855152f7f4845a0b976a6a0aa1d0121c0/dhu/captcha/code
 
-url = "http://webproxy.dhu.edu.cn/http/446a5061214023323032323131446855152f7f4845a0b976a6a0aa1d0121c0/dhu"
+url = "https://webproxy.dhu.edu.cn/https/446a5061214023323032323131446855152f7f4845a0b976a6a0aa1d0121c0/dhu"
+
 headers = {
-  'Accept': 'application/json, text/javascript, */*; q=0.01',
-  'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
-  'Cache-Control': 'no-cache',
-  'Connection': 'keep-alive',
-  'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-  'Cookie': input("cookie meow, give me cookie thanks meow: "),
-  'DNT': '1',
-  'Origin': 'https://webproxy.dhu.edu.cn',
-  'Pragma': 'no-cache',
-  'Sec-Fetch-Dest': 'empty',
-  'Sec-Fetch-Mode': 'cors',
-  'Sec-Fetch-Site': 'same-origin',
-  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.41',
-  'X-Requested-With': 'XMLHttpRequest',
-  'sec-ch-ua': '"Chromium";v="110", "Not A(Brand";v="24", "Microsoft Edge";v="110"',
-  'sec-ch-ua-mobile': '?0',
-  'sec-ch-ua-platform': '"Windows"',
+    'Accept': 'application/json, text/javascript, */*; q=0.01',
+    'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+    'Connection': 'keep-alive',
+    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+    'Cookie': input("cookie meow, give me cookie thanks meow: "),
+    'Origin': 'https://webproxy.dhu.edu.cn',
+    'Referer': 'https://webproxy.dhu.edu.cn/https/446a5061214023323032323131446855152f7f4845a0b976a6a0aa1d0121c0/dhu/selectcourse/toSCC',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-origin',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.37',
+    'X-Requested-With': 'XMLHttpRequest',
+    'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Microsoft Edge";v="114"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
 }
 
 cource_list_data = {
@@ -61,27 +63,27 @@ cource_list_data = {
   'bSortable_9': 'false',
 }
 
-def get_space_num(course_code: int, tc_code: int) -> int:
+def get_space_num(course_code: str, tc_code: str) -> int:
   while True:
     response = None
     try:
-      response = requests.post(
+      response = requests.post( 
         f'{url}/selectcourse/initACC?vpn-12-o1-jwgl.dhu.edu.cn',
         headers=dict(headers, Referer=f'{url}/selectcourse/toSH'),
         data=dict(cource_list_data, courseCode=course_code),
       )
       js: List[Dict[str, Any]] = json.loads(response.text)['aaData']
-      target = filter(lambda x: int(x['cttId']) == tc_code, js).__next__()
+      target = filter(lambda x: str(x['cttId']) == tc_code, js).__next__()
       remaining_quota = target['maxCnt']-target['enrollCnt']
       return remaining_quota
-    except KeyboardInterrupt:
+    except (KeyboardInterrupt, requests.exceptions.SSLError):
       raise
     except:
       if response is not None:
-        print(response.text)
+        op(response.json())
       traceback.print_exc()
 
-def del_cource(tc_code: int):
+def del_cource(tc_code: Union[None, str]):
   if not tc_code:
     return
   data = {
@@ -100,7 +102,7 @@ def del_cource(tc_code: int):
   else:
     print(f"退课<{tc_code}>:", response.text)
 
-def enroll(tc_code: int):
+def enroll(tc_code: str):
   data = {
     'cttId': tc_code,
     'needMaterial': False, 
@@ -123,7 +125,7 @@ def enroll(tc_code: int):
         print(response.text)
       traceback.print_exc()
 
-def try_enroll(course_code: int, tc_code: int, del_tc_code: int) -> bool:
+def try_enroll(course_code: str, tc_code: str, del_tc_code: Union[str, None]) -> bool:
   print(f'\r试抢{tc_code}'+f'  {random.random()}                  ', end='')
   if get_space_num(course_code, tc_code) > 0:
     del_cource(del_tc_code)
